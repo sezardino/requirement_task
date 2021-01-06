6709721fa8681faea0617de7876b0fb50f20a592
Edward <e.arechwa@gmail.com>
2021-01-06 16:25:20 +0100

    init swipper

--- src/assets/scripts/index.ts
+++ src/assets/scripts/index.ts
@@ -1,3 +1,33 @@
-export {};
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
 
-console.log('start');
+import Swiper, {Navigation, Pagination} from 'swiper';
+console.log();
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
+
+const mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  breakpoints: {
+    // // when window width is >= 320px
+    480: {
+      slidesPerView: 1,
+    },
+    // // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // // when window width is >= 640px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
